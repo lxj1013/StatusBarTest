83a4b9fdedaadfdc742c2d537c8684b2cccbe60c
lxj <lixinjian@tech-source.com.cn>
2018-12-27 15:37:55 +0800

    Initial commit

---
+++ app/src/main/java/cn/com/hisistar/statusbartest/StatusFragment.java
@@ -0,0 +1,251 @@
+package cn.com.hisistar.statusbartest;
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.EthernetDataTracker;
+import android.net.NetworkInfo;
+import android.net.ethernet.EthernetManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * DATE 2018/12/14 10:41
+ * AUTHOR lxj
+ * 南无阿弥陀佛
+ */
+public class StatusFragment extends Fragment implements View.OnClickListener {
+    private static final String TAG = StatusFragment.class.getSimpleName();
+
+    private ImageButton wifiView;
+    private ImageButton ethernetView;
+    private ImageButton bluetoothView;
+    private ImageButton hotspotView;
+    private StatusReceiver mReceiver;
+
+    private EthernetManager mEthernetManager;
+
+    private WifiManager mWifiManager;
+
+    private BluetoothAdapter mBluetoothAdapter;
+
+    private TextView tempTv;
+    private TextView weatherTv;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.status_bar, container, false);
+
+        initViews(view);
+
+        registerStatusReceiver();
+
+        initEthernetStatus();
+        initBluetoothStatus();
+        initWifiAPStatus();
+
+        initWeatherStatus();
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+
+
+        super.onStart();
+    }
+
+    @Override
+    public void onDestroy() {
+
+        super.onDestroy();
+        getActivity().unregisterReceiver(mReceiver);
+    }
+
+
+    private void initEthernetStatus() {
+        mEthernetManager = (EthernetManager) getActivity().getSystemService("ethernet");
+        if (mEthernetManager == null)
+            return;
+        if (mEthernetManager.getEthernetState() == EthernetManager.ETHERNET_STATE_ENABLED) {
+            ethernetView.setVisibility(View.VISIBLE);
+            if (mEthernetManager.getEthernetMode().equals(EthernetManager.ETHERNET_CONNECT_MODE_DHCP)) {
+                ethernetView.setImageResource(R.drawable.ethernet_on);
+            } else
+                ethernetView.setImageResource(R.drawable.ethernet_off);
+        } else {
+            ethernetView.setVisibility(View.GONE);
+        }
+    }
+
+    private void initViews(View view) {
+        wifiView = (ImageButton) view.findViewById(R.id.wifi_img);
+        ethernetView = (ImageButton) view.findViewById(R.id.ethernet_img);
+        bluetoothView = (ImageButton) view.findViewById(R.id.bluetooth_img);
+        hotspotView = (ImageButton) view.findViewById(R.id.hotspot_img);
+
+        tempTv = (TextView) view.findViewById(R.id.temp);
+        weatherTv = (TextView) view.findViewById(R.id.weather_txt);
+
+        wifiView.setOnClickListener(this);
+        ethernetView.setOnClickListener(this);
+        bluetoothView.setOnClickListener(this);
+        hotspotView.setOnClickListener(this);
+    }
+
+
+    private void initBluetoothStatus() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null)
+            return;
+        int state = mBluetoothAdapter.getState();
+        bluetoothView.setVisibility((state == BluetoothAdapter.STATE_ON) ? View.VISIBLE : View.GONE);
+        Log.e(TAG, "initViews: mBluetoothAdapter.getState()=" + state);
+    }
+
+    private void initWifiAPStatus() {
+        mWifiManager = (WifiManager) getActivity().getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        if (mWifiManager == null)
+            return;
+        int state = mWifiManager.getWifiState();
+        hotspotView.setVisibility((state == WifiManager.WIFI_AP_STATE_ENABLED) ? View.VISIBLE : View.GONE);
+        Log.e(TAG, "initViews: wifiAPState= " + mWifiManager.getWifiApState());
+    }
+
+    private void initWeatherStatus() {
+        Intent intent = new Intent(getActivity(), WeatherAutoUpdateService.class);
+        getActivity().startService(intent);
+    }
+
+
+    private void registerStatusReceiver() {
+        mReceiver = new StatusReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(EthernetManager.ETHERNET_STATE_CHANGED_ACTION);
+        filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+        filter.addAction(WifiManager.RSSI_CHANGED_ACTION);
+        filter.addAction(WifiManager.WIFI_AP_STATE_CHANGED_ACTION);
+        filter.addAction(WeatherAutoUpdateService.ACTION_REPORT_WEATHER);
+        getActivity().registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); //添加启动外部应用的Flag，不然会报错
+        intent.putExtra(PreferenceActivity.EXTRA_NO_HEADERS, true);
+        intent.setClassName("com.android.settings", "com.android.settings.Settings");//包名，要启动fragment所依赖的Activity
+        switch (v.getId()) {
+            case R.id.wifi_img:
+                intent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, "com.android.settings.wifi.WifiSettings");//要启动的fragment
+                startActivity(intent);
+                Toast.makeText(getActivity(), "wifi", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.ethernet_img:
+                intent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, "com.android.settings.EthernetSettings");//要启动的fragment
+                startActivity(intent);
+                Toast.makeText(getActivity(), "ethernet", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bluetooth_img:
+                intent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, "com.android.settings.bluetooth.BluetoothSettings");//要启动的fragment
+                startActivity(intent);
+                Toast.makeText(getActivity(), "bluetooth", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.hotspot_img:
+                intent.putExtra(PreferenceActivity.EXTRA_SHOW_FRAGMENT, "com.android.settings.TetherSettings");//要启动的fragment
+                startActivity(intent);
+                Toast.makeText(getActivity(), "hotspot", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(getActivity(), "error!!!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    private class StatusReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.e(TAG, "onReceive: " + intent.getAction());
+            String action = intent.getAction();
+            if (action == null)
+                return;
+            int message = -1;
+            int rel = -1;
+            if (action.equals(EthernetManager.ETHERNET_STATE_CHANGED_ACTION)) {
+                message = intent.getIntExtra(EthernetManager.EXTRA_ETHERNET_STATE, rel);
+
+                if (message == EthernetDataTracker.EVENT_DHCP_CONNECT_SUCCESSED || message == EthernetDataTracker.EVENT_STATIC_CONNECT_SUCCESSED) {
+                    ethernetView.setImageResource(R.drawable.ethernet_on);
+                } else {
+                    ethernetView.setImageResource(R.drawable.ethernet_off);
+                }
+
+                Log.e(TAG, "onReceive: " + EthernetManager.ETHERNET_STATE_CHANGED_ACTION + " message=" + message);
+            }
+
+            if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+                NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+                boolean wifiState = info.isConnected();
+                wifiView.setVisibility(wifiState ? View.VISIBLE : View.GONE);
+                Log.e(TAG, "onReceive: " + WifiManager.NETWORK_STATE_CHANGED_ACTION + " wifiState=" + wifiState);
+            }
+
+
+            if (action.equals(WifiManager.RSSI_CHANGED_ACTION)) {
+                WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                wifiView.setImageLevel(WifiManager.calculateSignalLevel(wifiInfo.getRssi(), 4));
+                Log.e(TAG, "onReceive: " + WifiManager.RSSI_CHANGED_ACTION + " rssi=" + wifiInfo.getRssi());
+            }
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                bluetoothView.setVisibility(state == BluetoothAdapter.STATE_ON ? View.VISIBLE : View.GONE);
+                Log.e(TAG, "onReceive: " + BluetoothAdapter.ACTION_STATE_CHANGED + " state =" + state);
+            }
+
+            if (action.equals(WifiManager.WIFI_AP_STATE_CHANGED_ACTION)) {
+                int state = intent.getIntExtra(WifiManager.EXTRA_WIFI_AP_STATE, WifiManager.WIFI_AP_STATE_FAILED);
+                hotspotView.setVisibility(state == WifiManager.WIFI_AP_STATE_ENABLED ? View.VISIBLE : View.GONE);
+                Log.e(TAG, "onReceive: " + WifiManager.WIFI_AP_STATE_CHANGED_ACTION + " state=" + state);
+            }
+
+            if (action.equals(WeatherAutoUpdateService.ACTION_REPORT_WEATHER)) {
+                String weatherInfo = intent.getStringExtra("info");
+                Log.e(TAG, "onReceive: weatherInfo=" + weatherInfo);
+                String weatherInfos[] = weatherInfo.split(";");
+                if (weatherInfos.length == 4) {
+                    tempTv.setVisibility(View.VISIBLE);
+                    weatherTv.setVisibility(View.VISIBLE);
+                    String temp = weatherInfos[1]+"℃";
+                    tempTv.setText(temp);
+                    int index = Integer.parseInt(weatherInfos[3]);
+                    String weather = getResources().getStringArray(R.array.weather)[index];
+                    weatherTv.setText(weather);
+                } else {
+                    tempTv.setVisibility(View.GONE);
+                    weatherTv.setVisibility(View.GONE);
+                }
+            }
+
+        }
+    }
+
+
+}
---
+++ app/src/main/java/cn/com/hisistar/statusbartest/WeatherAutoUpdateService.java
@@ -0,0 +1,170 @@
+package cn.com.hisistar.statusbartest;
+
+import android.annotation.SuppressLint;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.SystemClock;
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class WeatherAutoUpdateService extends Service {
+    private static final String TAG = "WeatherAutoUpdate";
+    public static final String ACTION_REPORT_WEATHER = "cn.com.hisistar.action.ReportWeather";
+    int count = 0;
+    int times = 10;
+
+    public WeatherAutoUpdateService() {
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+//        updateWeather();
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                updateWeather();
+            }
+        }.start();
+
+        updateOnTime(10);
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void updateOnTime(int time) {
+
+        AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+//        int anHour = 8 * 60 * 60 * 1000; // 这是8小时的毫秒数
+        int anHour = time * 60 * 1000; // 这是1小时的毫秒数
+        Log.e(TAG, "onStartCommand: elapsedRealtime = " + SystemClock.elapsedRealtime());
+        long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
+        Intent i = new Intent(this, WeatherAutoUpdateService.class);
+        PendingIntent pi = PendingIntent.getService(this, 0, i, 0);
+        manager.cancel(pi);
+        manager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
+    }
+
+    private void updateWeather() {
+        Log.i(TAG, "Loading location infomation ...");
+        String ipJson = getHttpEntity("http://ip-api.com/json");
+        Log.e(TAG, "onHandleIntent: ipjson=" + ipJson);
+        if (ipJson == null)
+            return;
+        String location = getLocationFromJson(ipJson);
+        Log.e(TAG, "onHandleIntent: location=" + location);
+        if (location == null)
+            return;
+
+        Log.i(TAG, "Querying weather infomation ...");
+        String url = buildQueryUrl(location);
+        Log.e(TAG, "onHandleIntent: url= " + url);
+        String weatherJson = getHttpEntity(url);
+        if (weatherJson == null)
+            return;
+
+        Log.i(TAG, "Report weather infomation ...");
+        String weatherInfo = getWeatherInfo(weatherJson);
+        count++;
+        times = 10;
+        Log.e(TAG, "updateWeather: count=" + count);
+        Intent intentWeather = new Intent(ACTION_REPORT_WEATHER);
+        // info "city;temp;text;code"
+        intentWeather.putExtra("info", weatherInfo);
+        sendBroadcast(intentWeather);
+    }
+
+    private String getHttpEntity(String url) {
+        HttpGet getMethod = new HttpGet(url);
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpResponse response;
+        try {
+            response = httpClient.execute(getMethod);
+        } catch (IOException e) {
+            e.printStackTrace();
+            if (times > 0)
+                updateOnTime(1);
+            times--;
+            return null;
+        }
+        if (response.getStatusLine().getStatusCode() == 200) {
+            try {
+                return EntityUtils.toString(response.getEntity(), "utf-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        return null;
+    }
+
+
+    private String getLocationFromJson(String json) {
+        try {
+            JSONObject jObject = new JSONObject(json);
+            return jObject.getString("city") + ", " + jObject.getString("countryCode");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private String buildQueryUrl(String location) {
+        List<BasicNameValuePair> params = new LinkedList<BasicNameValuePair>();
+        params.add(new BasicNameValuePair("q", "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"" + location + "\")"));
+        params.add(new BasicNameValuePair("format", "json"));
+        String param = URLEncodedUtils.format(params, "UTF-8");
+        String baseUrl = "https://query.yahooapis.com/v1/public/yql";
+        return baseUrl + "?" + param;
+    }
+
+    private String getWeatherInfo(String json) {
+        Log.i(TAG, "getWeatherInfo");
+        Log.e(TAG, "getWeatherInfo: json=" + json);
+        try {
+            JSONObject jObjectYahoo = new JSONObject(json);
+            JSONObject channelObject = jObjectYahoo
+                    .getJSONObject("query")
+                    .getJSONObject("results")
+                    .getJSONObject("channel");
+            JSONObject locationObject = channelObject.getJSONObject("location");
+            String text = locationObject.getString("city");
+            JSONObject conditionObject = channelObject.getJSONObject("item").getJSONObject("condition");
+            // 摄氏度(℃)=（华氏度(℉)-32）÷1.8
+            text += ";" + String.valueOf((int) ((conditionObject.getInt("temp") - 32) / 1.8));
+            text += ";" + conditionObject.getString("text");
+            text += ";" + conditionObject.getString("code");
+            Log.i(TAG, "weather info (city;temp;text;code) : " + text);
+            return text;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
